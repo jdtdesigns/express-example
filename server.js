964fab333228189c6cca1bf02bc1d995309072cc
JD Tadlock <jt7903@gmail.com>
2023-04-05 10:42:49 -0400

    added evn

---
+++ server.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+const express = require('express');
+const PORT = process.env.PORT;
+
+// const app = express();
+
+console.log(PORT);
+console.log(process.env.API_KEY);
+
+
+
+
+
